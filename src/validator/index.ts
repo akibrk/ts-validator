bc10209bd95ad56e6517359b77b94991cf675de8
Akib Khan <akibrk.m@gmail.com>
2022-03-13 12:45:47 +0600

    Update rule

--- src/validator/index.ts
+++ src/validator/index.ts
@@ -3,28 +3,28 @@ import { Type } from '../types';
 import { isEmailAddress } from './utils';
 
 /**
- * Validates single property against a validation model
+ * Validates single property against a validation rule
  * @param obj to validate
  * @param propName name of the property
- * @param model Validation Model
+ * @param rule Validation Rule
  * @returns IValidationError | undefined
  */
-function validateThis(obj: any, propName: string, model: ValidateThis): ValidationError | undefined {
+function validateThis(obj: any, propName: string, rule: ValidateThis): ValidationError | undefined {
   let vError: ValidationError = {
     field: propName,
     errors: [],
   };
 
   // If there isn't a rule defined for the property ignore it
-  if (!model) {
-    return model;
+  if (!rule) {
+    return rule;
   }
 
-  if (model.allowNull) {
+  if (rule.allowNull) {
     return undefined;
   }
 
-  if (model.required) {
+  if (rule.required) {
     if (typeof obj === undefined || typeof obj === null) {
       vError.errors.push({
         message: `Field is required, missing field: ${propName}`,
@@ -32,11 +32,11 @@ function validateThis(obj: any, propName: string, model: ValidateThis): Validati
     }
   }
 
-  switch (model.type) {
+  switch (rule.type) {
     case Type.email:
       if (typeof obj !== Type.string) {
         vError.errors.push({
-          message: `Invalid type, expected ${model.type} got ${typeof obj}`,
+          message: `Invalid type, expected ${rule.type} got ${typeof obj}`,
         });
       } else {
         const { value, error } = isEmailAddress(obj);
@@ -46,57 +46,57 @@ function validateThis(obj: any, propName: string, model: ValidateThis): Validati
           });
         }
 
-        if (model.minLength) {
-          if (obj.length < model.minLength) {
+        if (rule.minLength) {
+          if (obj.length < rule.minLength) {
             vError.errors.push({
-              message: `Invalid ${model.type} address, must be equal or longer than ${model.minLength}`,
+              message: `Invalid ${rule.type} address, must be equal or longer than ${rule.minLength}`,
             });
           }
         }
-        if (model.maxLength) {
-          if (obj.length > model.maxLength) {
+        if (rule.maxLength) {
+          if (obj.length > rule.maxLength) {
             vError.errors.push({
-              message: `Invalid ${model.type} address, must be equal or shorter than ${model.maxLength}`,
+              message: `Invalid ${rule.type} address, must be equal or shorter than ${rule.maxLength}`,
             });
           }
         }
       }
       break;
     default:
-      if (model.type !== typeof obj) {
+      if (rule.type !== typeof obj) {
         vError.errors.push({
-          message: `Invalid type, expected ${model.type} got ${obj === null ? 'null' : typeof obj}`,
+          message: `Invalid type, expected ${rule.type} got ${obj === null ? 'null' : typeof obj}`,
         });
       } else {
-        if (model.type === Type.string) {
-          if (model.minLength) {
-            if (obj.length < model.minLength) {
+        if (rule.type === Type.string) {
+          if (rule.minLength) {
+            if (obj.length < rule.minLength) {
               vError.errors.push({
-                message: `Invalid '${propName}', must be equal or longer than ${model.minLength}`,
+                message: `Invalid '${propName}', must be equal or longer than ${rule.minLength}`,
               });
             }
           }
 
-          if (model.maxLength) {
-            if (obj.length > model.maxLength) {
+          if (rule.maxLength) {
+            if (obj.length > rule.maxLength) {
               vError.errors.push({
-                message: `Invalid '${propName}', must be equal or shorter than ${model.maxLength}`,
+                message: `Invalid '${propName}', must be equal or shorter than ${rule.maxLength}`,
               });
             }
           }
-        } else if (model.type === Type.number) {
-          if (model.min) {
-            if (obj < model.min) {
+        } else if (rule.type === Type.number) {
+          if (rule.min) {
+            if (obj < rule.min) {
               vError.errors.push({
-                message: `Invalid '${propName}', must be equal or larger than ${model.min}`,
+                message: `Invalid '${propName}', must be equal or larger than ${rule.min}`,
               });
             }
           }
 
-          if (model.max) {
-            if (obj > model.max) {
+          if (rule.max) {
+            if (obj > rule.max) {
               vError.errors.push({
-                message: `Invalid '${propName}', must be equal or less than ${model.max}`,
+                message: `Invalid '${propName}', must be equal or less than ${rule.max}`,
               });
             }
           }
@@ -108,7 +108,7 @@ function validateThis(obj: any, propName: string, model: ValidateThis): Validati
 }
 
 /**
- * Validates any object against a validation model
+ * Validates any object against a validation rule
  * @param obj to validate
  * @param model to validate against
  * @returns IValidationError[]
