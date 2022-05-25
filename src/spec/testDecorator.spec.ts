12bf390e3911cde561e7e12a7a95ca8f367dd2ff
Akib Khan <akibrk.m@gmail.com>
2022-05-25 21:26:24 +0600

    Test cases

--- src/spec/testDecorator.spec.ts
+++ src/spec/testDecorator.spec.ts
@@ -1,37 +1,40 @@
-import { minLength, ValidationError, VError } from '../index';
+import { minLength, VError } from '../index';
 describe('String validation', () => {
   it('should pass', () => {
     expect(1 + 1).toBe(2);
   });
 
-  class TestDecorator {
-    @minLength(2)
-    public checkMinPass: string;
-    @minLength(100)
-    public checkMinFail: string;
-
-    @minLength(1)
-    public checkMinArrayPass: Array<string>;
-    @minLength(100)
-    public checkMinArrayFail: Array<string>;
-
-    constructor() {
-      this.checkMinPass = 'test';
-      this.checkMinFail = 'test';
-      this.checkMinArrayFail = ['test'];
-      this.checkMinArrayPass = ['test'];
-    }
-  }
-
-  it('Should fail ', () => {
-    try {
-      const TEST_INSTANCE = new TestDecorator();
-    } catch (error) {
-      if (error instanceof VError) {
-        console.log(error);
-      } else {
-        console.error(error);
+  it('Check min pass ', () => {
+    class CaseClass {
+      @minLength(2)
+      public checkMin: string;
+      @minLength(1)
+      public checkMinArray: Array<string>;
+      constructor() {
+        this.checkMin = 'test';
+        this.checkMinArray = ['test'];
       }
+    }
+    const case_instance = new CaseClass();
+    expect(case_instance).not.toBeUndefined();
+  });
+
+  it('Check min fail ', () => {
+    class CaseClass {
+      @minLength(100)
+      public checkMin: string;
+      @minLength(100)
+      public checkMinArray: Array<string>;
+      constructor() {
+        this.checkMin = 'test';
+        this.checkMinArray = ['test'];
+      }
+    }
+    try {
+      new CaseClass();
+    } catch (error) {
+      expect(error instanceof VError).toBeTruthy();
+      expect((error as VError).field).toBe('checkMin');
     }
   });
 });
